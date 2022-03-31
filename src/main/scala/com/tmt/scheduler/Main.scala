6aa3b100eef5dffa3bb968f6e0a595c3271c2d5b
Shubham Chauhan <shubham25may1998@gmail.com>
2022-03-31 14:22:51 +0530

    Implementation of scheduler with one thread per task(and one task/scheduler)

--- src/main/scala/com/tmt/scheduler/Main.scala
+++ src/main/scala/com/tmt/scheduler/Main.scala
@@ -1,36 +1,36 @@
 package com.tmt.scheduler
 
-import java.time.temporal.ChronoUnit
-import java.time.{Duration, Instant}
+import java.time.Instant
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
-
 class TimeScheduler {
 
-  private var stop:Boolean = false
-  private def nextInterval(starTime: Instant) = {
-    val currentTime = Instant.now()
-    if (starTime.compareTo(currentTime) >= 0) {
-      Duration.between(currentTime, starTime)
-    } else Duration.ofMillis(0)
-  }
+  private var stop: Boolean = false
+//  private def nextInterval(starTime: Instant) = {
+//    val currentTime = Instant.now()
+//    if (starTime.compareTo(currentTime) >= 0) {
+//      Duration.between(currentTime, starTime)
+//    } else Duration.ofMillis(0)
+//  }
 
   private def scheduleWithin(intervalInMillis: Int)(task: Runnable): Unit = {
-    println("hello I'm here")
+    var waitTicks = 0
+
     while (!stop) {
-      task.run()
-      val startTime = Instant.now().plus(intervalInMillis, ChronoUnit.MILLIS)
-      val interval = nextInterval(startTime)
-      val nano = interval.getNano
-      val millisInterval = interval.getSeconds * 1000 + (nano / (1000 * 1000))
-      Thread.sleep(millisInterval, nano % (1000 * 1000))
+      if (waitTicks == 0) {
+        task.run()
+        waitTicks = intervalInMillis
+      }
+      val sleepIntervalInNano = 1000 * 1000 - Instant.now().getNano % 1000
+//      println(sleepInterval)
+      Thread.sleep(sleepIntervalInNano / (1000 * 1000), sleepIntervalInNano % (1000 * 1000))
+      waitTicks -= 1
     }
   }
 
   def schedule(afterInMillis: Int, intervalInMillis: Int)(task: => Unit) = {
     val unit: Runnable = () => {
-      println("hello")
       Thread.sleep(afterInMillis)
       scheduleWithin(intervalInMillis)(() => task)
     }
@@ -48,11 +48,11 @@ object Main extends App {
 
   private val times: ArrayBuffer[Instant] = mutable.ArrayBuffer.empty
 
-  scheduler.schedule(10, 1) {
+  scheduler.schedule(10, 10) {
     times.append(Instant.now())
   }
 
-  Thread.sleep(1000)
+  Thread.sleep(10000)
   scheduler.cancel()
-  println(times.size)
+  println(times.map(x => s"${x.getEpochSecond} ${x.getNano}").mkString("\n"))
 }
